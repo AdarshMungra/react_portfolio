084bf6604ac467f3f28c1b3aea6833c41a0a7b51
AdarshMungra <adarshmungra1@gmail.com>
2023-10-21 20:02:21 +0400

    skills fix

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,13 +1,34 @@
 // Skills.js
 import React from 'react';
 import './skills.css'; // Import the CSS file
-import WordCloud from './WordCloud'; // Import the WordCloud component
 
 const Skills = () => {
   return (
     <div className="skills-container">
       <h1 className="skills-heading">My Skills</h1>
-      <WordCloud /> {/* Use the WordCloud component here */}
+
+      <div className="skills-category">
+        <h2 className="skills-category-heading">Front-end development</h2>
+        <p>HTML, CSS, JavaScript</p>
+        <p>React, Angular, Vue.js</p>
+        <p>UI/UX design</p>
+      </div>
+
+      <div className="skills-category">
+        <h2 className="skills-category-heading">Programming Languages</h2>
+        <p>JavaScript</p>
+        <p>Python</p>
+        <p>Java</p>
+        <p>C++</p>
+      </div>
+
+      <div className="skills-category">
+        <h2 className="skills-category-heading">Tools and Technologies</h2>
+        <p>Git and GitHub</p>
+        <p>Webpack</p>
+        <p>Node.js</p>
+        <p>SQL and NoSQL databases</p>
+      </div>
     </div>
   );
 }
