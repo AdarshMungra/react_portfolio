f064104149330df5468f7d8ba58d5bcfd03c1961
AdarshMungra <adarshmungra1@gmail.com>
2023-10-22 16:22:59 +0400

    fix cursor  ball

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,55 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import './home.css';
+import CursorBall from '../cursorBall/cursorBall'; // Import the CursorBall component
 
 const Home = () => {
-  const ballRef = useRef(null);
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
-
-  const handleMouseMove = (event) => {
-    setMouseX(event.pageX);
-    setMouseY(event.pageY);
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  useEffect(() => {
-    let animationFrameId;
-
-    const animate = () => {
-      if (ballRef.current) {
-        const ballRect = ballRef.current.getBoundingClientRect();
-        const ballCenterX = ballRect.left + ballRect.width / 2;
-        const ballCenterY = ballRect.top + ballRect.height / 2;
-        const distX = mouseX - ballCenterX;
-        const distY = mouseY - ballCenterY;
-        const newBallX = ballRect.left + distX * 0.06;
-        const newBallY = ballRect.top + distY * 0.06;
-        ballRef.current.style.transform = `translate(${newBallX}px, ${newBallY}px)`;
-      }
-      animationFrameId = requestAnimationFrame(animate);
-    };
-
-    animationFrameId = requestAnimationFrame(animate);
-
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [mouseX, mouseY]);
-
   return (
     <div className="home">
       <section>
         <div className="text-content">
           <h1>HELLO</h1>
         </div>
-        <div className="ball" ref={ballRef}></div>
       </section>
+      <CursorBall /> {/* Include the CursorBall component */}
     </div>
   );
 };
